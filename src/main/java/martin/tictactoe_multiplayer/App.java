379c38b829cced7138d94dc3dfe9210795deb0d0
Martin Angelov <martin_angelov@epam.com>
2017-02-08 18:37:54 +0200

    first commit

---
+++ src/main/java/martin/tictactoe_multiplayer/App.java
@@ -0,0 +1,13 @@
+package martin.tictactoe_multiplayer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
